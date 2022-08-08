8eb85b59c34af45e78b6d7f58b48133b6f35e338
Godfrey Owidi <godfreyowiidi@gmail.com>
2022-08-08 22:32:56 +0300

    Add func to fetch USD values from API.

--- src/crypto.js
+++ src/crypto.js
@@ -125,3 +125,49 @@ var getPortfolioValuePerToken = function() {
         });
     });
 }
+
+// Function to fetch the USD Values from CryptoCompare
+function getUSDValues() {
+
+    var cryptoURL = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=3789ea397be622354552b3ab2a826e4379b5da952de997d3cff964ed4f0786ee';
+
+    var options = {
+        url: cryptoURL,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    // Return new promise 
+    return new Promise(function (resolve, reject) {
+        // Do async job
+        request.get(options, function (err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(JSON.parse(body));
+            }
+        })
+    })
+
+}
+
+// function filterByProperty(array, prop, value){
+//     var filtered = [];
+//     for(var i = 0; i < array.length; i++){
+
+//         var obj = array[i];
+
+//         for(var key in obj){
+//             if(typeof(obj[key] == "object")){
+//                 var item = obj[key];
+//                 if(item[prop] == value){
+//                     filtered.push(item);
+//                 }
+//             }
+//         }
+
+//     }    
+
+//     return filtered;
+
+// }
